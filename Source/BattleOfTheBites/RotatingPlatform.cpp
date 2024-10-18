e9823ea51fe02e61e8904fc6235fcc286ecf957d
jeetshah-410 <jeetshah0410@gmail.com>
2024-10-18 18:38:10 +0530

    Track large files with LFS

---
+++ Source/BattleOfTheBites/RotatingPlatform.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RotatingPlatform.h"
+
+// Sets default values
+ARotatingPlatform::ARotatingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARotatingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARotatingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
